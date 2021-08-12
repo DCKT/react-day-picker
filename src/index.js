1fe3e76b12dd3e33f08e830a5128486b8a8ce84c
Thomas Deconinck <thomas.deconinck@colisweb.com>
2021-08-12 18:34:10 +0200

    update import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-export { default } from './DayPicker';
+export { default as DayPicker } from './DayPicker';
+export { default as DayPickerInput } from './DayPickerInput';
 export { default as DateUtils } from './DateUtils';
 export { default as LocaleUtils } from './LocaleUtils';
 export { default as ModifiersUtils } from './ModifiersUtils';
