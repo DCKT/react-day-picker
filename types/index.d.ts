40efce78cea4147459606eda2bed2272b90b8663
Thomas Deconinck <thomas.deconinck@colisweb.com>
2021-08-12 18:44:10 +0200

    test

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
 export as namespace DayPicker;
 
-export { default } from './DayPicker';
+export { default as DayPicker } from './DayPicker';
+export { default as DayPickerInput } from './DayPickerInput';
 export * from './Props';
 export * from './LocaleUtils';
 export * from './DateUtils';
